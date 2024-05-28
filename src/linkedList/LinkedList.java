26a9ba6ba93f2c73cd25e5e312ff499321931af8
Vikash1902 <vikashmishra.m11@gmail.com>
2024-05-28 23:09:29 +0530

    add element at statring of linked list 'prepend'

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -39,4 +39,18 @@ public class LinkedList {
         }
         length++;
     }
+
+    // add at first
+
+    public void prepend(int value){
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        }else{
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
 }
--- src/linkedList/LinkedListMain.java
+++ src/linkedList/LinkedListMain.java
@@ -13,10 +13,30 @@ public class LinkedListMain {
 
         newLinkedList.print();
 
+        System.err.println("------------------------");
         // append new node
 
         newLinkedList.append(2);
 
         newLinkedList.print();
+
+        System.err.println("length " + newLinkedList.getLength());
+
+        System.err.println("------------------------");
+        // prepend new node
+
+        newLinkedList.append(3);
+
+        newLinkedList.prepend(0);
+
+        newLinkedList.print();
+
+        System.err.println("head " + newLinkedList.getHead().value);
+
+        System.err.println("tail " + newLinkedList.getTail().value);
+
+        System.err.println("length " + newLinkedList.getLength());
+
+        System.err.println("------------------------");
     }
 }
